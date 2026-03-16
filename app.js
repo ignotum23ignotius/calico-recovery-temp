6f9bdbcfefe80ab9e69d638d1bf6945262787487
ignotumignotius23 <ignotumignotius23@gmail.com>
2026-03-16 04:59:24 -0500

    tryagain
    lol

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const express = require('express');
+const app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+const http = require('http');
 const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+var express = require("express"); 
+var app = express(); 
+var path = require("path"); 
 
 const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   const path = require('node:path');
   const index = '/pages/index.html';
   const fs = require('node:fs');
-  fs.readFile('/pages.index.html', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+  app.get('/',function(req,res){ 
+    res.sendFile(path.join(__dirname+'/index.html')); 
+    //__dirname : It will resolve to your project folder. 
+  }); 
+app.listen(3000); 
+console.log("Server running at Port 3000"); 
   console.log(data);
 });
 });
