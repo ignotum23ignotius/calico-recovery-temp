103416f62e2d12bdbcb86894adac922d53e519f7
ignotumignotius23 <ignotumignotius23@gmail.com>
2026-03-16 04:44:15 -0500

    herewegoagain
    gogogo

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const { createServer } = require('node:http');
 
-const hostname = 'calico-recoveru-b6bbb7b2f8bshzdq.swedencentral-01.azurewebsites.net';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  const path = require('node:path');
+  const index = '/pages/index.html';
+  const fs = require('node:fs');
+  fs.readFile('/pages.index.html', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
 });
 
 server.listen(port, hostname, () => {
